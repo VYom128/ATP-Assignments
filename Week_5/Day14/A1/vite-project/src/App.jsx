76efc75b8bb389f71eeca96c59f93802f813fbaa
Vyomakesh <tvyomakesh2023@gmail.com>
2026-04-03 19:59:18 +0530

    Rename files and update code logic

--- Week_5/Day14/A1/vite-project/src/App.jsx
+++ Week_5/Day14/A1/vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import Product from "./product.jsx"
+import Product from "./Components/Product"
 
 function App() {
 
--- Week_5/Day14/A1/vite-project/src/Components/Product.jsx
+++ Week_5/Day14/A1/vite-project/src/Components/Product.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 function Product({ product }) {
   return (
     <div className="border rounded-lg p-4 shadow">
